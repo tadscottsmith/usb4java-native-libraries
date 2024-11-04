8ad86b16725c72fa30fc82cf0287de0b4a4bf5d5
Dennis Sheirer <dsheirer@github.com>
2024-11-04 03:53:03 -0500

    Package publishing.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,25 @@
 plugins {
     id("java")
+    `maven-publish`
 }
 
 group = "io.github.dsheirer"
 version = "1.3.1"
+
+publishing {
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/dsheirer/usb4java-native-libraries")
+            credentials {
+                username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
+                password = project.findProperty("gpr.token") as String? ?: System.getenv("TOKEN")
+            }
+        }
+    }
+    publications {
+        register<MavenPublication>("gpr") {
+            from(components["java"])
+        }
+    }
+}
